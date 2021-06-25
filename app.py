327214bf43693c11c165e367539bab22ffadc7b4
Irmuun <rokahakor@gmail.com>
2021-06-24 17:44:47 -0700

    added parser

--- app.py
+++ app.py
@@ -1,24 +1,57 @@
+import xml_parser
 import json
-import re
-import xml.etree.ElementTree as ET
+import json_api_doc
 import flask
 from flask import Flask
 
 app = Flask(__name__)
-
+data = {}
 
 @app.route("/")
 @app.route('/files/<filename>')
 def index(filename=None):
-    return flask.render_template('page.html', filename=filename)
+    if filename is not None:
+        json_text = data[filename]
+        return flask.render_template('page.html', filename=filename, json_text=json_text)
+    return flask.render_template('page.html')
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/api/upload', methods=['POST'])
 def upload():
-    if flask.request.method == 'POST':
-        f = flask.request.files['filename']
-        return flask.redirect('/files/'+f.filename)
+    f = flask.request.files['filename']
+    parsed_tuple = xml_parser.parse_xml(f)
+    document = {
+        '$type': 'article',
+        'id': '1',
+        'name': 'Article 1'
+    }
+    print(json_api_doc.serialize(document))
+    save_database()
+    return flask.redirect('/files/'+f.filename)
+
+
+@app.route('/api/file/<filename>', methods=['GET'])
+def retrieve(filename):
+    parsed_file = data[filename]
+    return parsed_file
+
+
+def load_database():
+    global data
+    try:
+        with open('database.txt') as json_file:
+            data = json.load(json_file)
+    except FileNotFoundError:
+        with open('database.txt', 'w') as outfile:
+            json.dump(data, outfile)
+
+
+def save_database():
+    global data
+    with open('database.txt', 'w') as outfile:
+        json.dump(data, outfile)
 
 
 if __name__ == '__main__':
+    load_database()
     app.run(debug=True)
---
+++ xml_parser.py
@@ -0,0 +1,58 @@
+import re
+import xml.etree.ElementTree as ET
+
+def namespace(element):
+    m = re.match(r'\{.*\}', element.tag)
+    return m.group(0) if m else ''
+
+
+def parse_text(root):
+    block = []
+    for item in root.findall('.//{http://www.abbyy.com/FineReader_xml/FineReader10-schema-v1.xml}formatting'):
+        if item.text:
+            block.append(item.text)
+    return block
+
+
+def find_text(text_list, text):
+    for index, value in enumerate(text_list):
+        if text in value:
+            return index
+
+
+def find_text_reverse(text_list, text, start_index):
+    for index in range(start_index, -1, -1):
+        if text in text_list[index]:
+            return index
+
+
+def find_vs(text_list, defend_index):
+    for index in range(defend_index, -1, -1):
+        if re.search('[Vv][Ss]{0,1}\.', text_list[index]):
+            return index
+
+
+def find_defendant(text_list, start, end):
+    text = ''
+    for index in range(start, end + 1):
+        if ',' in text_list[index] and len(text_list[index]) > 3:
+            text += text_list[index] + '\n'
+    return text
+
+
+def find_plaintiff(text_list, start):
+    for index in range(start - 1, -1, -1):
+        if ',' in text_list[index] and len(text_list[index]) > 3:
+            return text_list[index]
+
+
+def parse_xml(xml):
+    tree = ET.parse(xml)
+    root = tree.getroot()
+    parse_list = parse_text(root)
+    defend_index = find_text(parse_list, 'Defendant')
+    vs_index = find_vs(parse_list, defend_index)
+    plaint_index = find_text_reverse(parse_list, 'Plaintiff,', vs_index)
+    plaintiff = find_plaintiff(parse_list, plaint_index)
+    defendant = find_defendant(parse_list, vs_index, defend_index)
+    return (plaintiff, defendant)
